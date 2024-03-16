9ddc24fb815908fa6bdab751e7729fce751536b9
Harutyunyan2001 <143323799+Harutyunyan2001@users.noreply.github.com>
2024-03-16 13:17:56 +0400

    Add files via upload

---
+++ LinearSVC.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC
+
+
+iris_dataset = load_iris() 
+X_train, X_test, y_train, y_test = train_test_split(iris_dataset['data'], iris_dataset['target'], random_state=0) 
+
+model = LinearSVC(C=10)
+model.fit(X_train, y_train) 
+X_new = np.array([[5, 2.9, 1, 0.2]]) 
+print("форма массива X_new: {}".format(X_new.shape)) 
+prediction = model.predict(X_new) 
+print("Прогноз: {}".format(prediction)) 
+print("Спрогнозированная метка: {}".format(iris_dataset['target_names'][prediction])) 
+
+print("train score",model.score(X_train,y_train))
+print("test score",model.score(X_test,y_test))
---
+++ LogisticRegression_classification.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+iris_dataset = load_iris() 
+X_train, X_test, y_train, y_test = train_test_split(iris_dataset['data'], iris_dataset['target'], random_state=0) 
+
+ 
+model = LogisticRegression(C=10)   
+model.fit(X_train, y_train) 
+
+X_new = np.array([[5, 2.9, 1, 0.2]]) 
+print("форма массива X_new: {}".format(X_new.shape)) 
+prediction = model.predict(X_new) 
+print("Прогноз: {}".format(prediction)) 
+print("Спрогнозированная метка: {}".format(iris_dataset['target_names'][prediction])) 
+
+
+print("train score",model.score(X_train,y_train))
+print("test score",model.score(X_test,y_test))
